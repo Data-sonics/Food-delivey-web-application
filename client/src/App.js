bd05f899b311897b56a3e209e6018d7cb3ea6e83
temvvlen1211 <temvvlen1211@gmail.com>
2023-04-19 19:56:05 +0800

    navbar footer negtgev

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import Home from "./pages/Home";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import "./style/index.css";
-=======
-import "./styles/index.css";
->>>>>>> main
-=======
-import "./styles/index.css";
->>>>>>> a78088542a61a6a3ebe24147ab0300b8cf616915
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
