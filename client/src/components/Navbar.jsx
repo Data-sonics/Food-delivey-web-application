49038b6f0d93dc85fdde90e061a00d64475fbc1f
ermun123 <oyunbayarermun@gmail.com>
2023-04-21 16:19:27 +0800

    fixed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import Logo from "./Logo";
 import { FaShoppingBag } from "react-icons/fa";
 
 export default function Navbar() {
+  const topchoices = ["Home", "About us", "Restaurants", "Pages", "Contacts"];
   return (
     <nav className="bg-gradient">
       <div className="container mx-auto py-4">
@@ -9,31 +10,13 @@ export default function Navbar() {
           <Logo />
           <div className="items-center flex justify-between w-full">
             <ul className="flex ms-16 font-thin p-4 md:p-0rounded-lg  mt-0 border-0 space-x-8 text-xl">
-              <li>
-                <a href="/" className="hover:text-amber-500 duration-300">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-amber-500 duration-300">
-                  About us
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-amber-500 duration-300">
-                  Restaurants
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-amber-500 duration-300">
-                  Pages
-                </a>
-              </li>
-              <li>
-                <a href="/" className="hover:text-amber-500 duration-300">
-                  Contacts
-                </a>
-              </li>
+              {topchoices.map((item, index) => (
+                <li key={index}>
+                  <a href="/" className="hover:text-amber-500 duration-300">
+                    {item}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="flex p-2">
