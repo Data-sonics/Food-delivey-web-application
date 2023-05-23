9547c04fcafb820f9042dde955a9b2f9ff4eb776
temvvlen1211 <temvvlen1211@gmail.com>
2023-05-23 18:51:27 +0800

    zarim zuiliig yanzlav

--- client/src/components/home/HowItWork.jsx
+++ client/src/components/home/HowItWork.jsx
@@ -22,55 +22,62 @@ export default function Howitworks() {
     });
   }, []);
   return (
-    <section className="mb-10">
-      <div className="text-center pt-16">
+    <section className="container mx-auto my-10">
+      <div className="text-center grid gap-5 ">
         <h1 className="text-5xl font-bold">How it works</h1>
-        <p className="w-[600px] mx-auto text-gray-600 pt-5 font-medium">
+        <p className=" mx-auto text-gray-600  font-medium">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
           dignissimos earum autem vel eveniet nisi dolorem doloribus?
         </p>
-      </div>
-      <div className="flex justify-center pt-10 gap-[150px]">
-        <div data-aos="flip-up">
-          <img
-            src="https://bslthemes.com/html/quickeat/assets/img/illustration-1.png"
-            alt="select"
-          />
-          <h1 className="text-2xl pt-5">
-            <span className="text-5xl text-gray-400">01</span>
-            Select Restaurant
-          </h1>
-          <p className="w-[280px] text-center text-gray-500 pt-5">
-            Non enim praesent elementum facilisis leo vel fringilla. Lectus
-            proin nibh nisl condimentum id. Quis varius quam quisque id diam
-            vel.
-          </p>
-        </div>
-        <div data-aos="flip-up">
-          <img
-            src="https://bslthemes.com/html/quickeat/assets/img/illustration-2.png"
-            alt="menu"
-          />
-          <h1 className="text-2xl pt-5">
-            <span className="text-5xl text-gray-400">02</span> Select Menu
-          </h1>
-          <p className="w-[280px] text-center text-gray-500 pt-5">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis
-            voluptate quibusdam ex eius eaque voluptatem voluptatum doloremque!
-          </p>
-        </div>
-        <div data-aos="flip-up">
-          <img
-            src="https://bslthemes.com/html/quickeat/assets/img/illustration-3.png"
-            alt=""
-          />
-          <h1 className="text-2xl pt-5">
-            <span className="text-5xl text-gray-400">03</span> Wait for delivery
-          </h1>
-          <p className="w-[280px] text-center text-gray-500 pt-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-            reiciendis pariatur fugit illum nulla hic.
-          </p>
+        <div className="grid justify-center items-center  gap-20 md:flex md:justify-between">
+          <div data-aos="flip-up" className="w-full max-w-sm">
+            <img
+              src="https://bslthemes.com/html/quickeat/assets/img/illustration-1.png"
+              className="w-full"
+              alt="select"
+            />
+            <p className="text-2xl text-center ">
+              <span className="text-5xl  text-gray-400">01</span>
+              Select Restaurant
+            </p>
+            <p className=" text-center text-gray-500 pt-5">
+              Non enim praesent elementum facilisis leo vel fringilla. Lectus
+              proin nibh nisl condimentum id. Quis varius quam quisque id diam
+              vel.
+            </p>
+          </div>
+          <div data-aos="flip-up" className="w-full max-w-sm">
+            <img
+              src="https://bslthemes.com/html/quickeat/assets/img/illustration-2.png"
+              className="w-full"
+              alt="select"
+            />
+            <p className="text-2xl text-center ">
+              <span className="text-5xl  text-gray-400">02</span>
+              Select Restaurant
+            </p>
+            <p className=" text-center text-gray-500 pt-5">
+              Non enim praesent elementum facilisis leo vel fringilla. Lectus
+              proin nibh nisl condimentum id. Quis varius quam quisque id diam
+              vel.
+            </p>
+          </div>
+          <div data-aos="flip-up" className="w-full max-w-sm">
+            <img
+              src="https://bslthemes.com/html/quickeat/assets/img/illustration-3.png"
+              className="w-full"
+              alt="select"
+            />
+            <p className="text-2xl text-center ">
+              <span className="text-5xl  text-gray-400">03</span>
+              Select Restaurant
+            </p>
+            <p className=" text-center text-gray-500 pt-5">
+              Non enim praesent elementum facilisis leo vel fringilla. Lectus
+              proin nibh nisl condimentum id. Quis varius quam quisque id diam
+              vel.
+            </p>
+          </div>
         </div>
       </div>
     </section>
