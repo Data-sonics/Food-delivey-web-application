21b44a92a822eb8dc186768e112041aec4409b81
temvvlen1211 <temvvlen1211@gmail.com>
2023-05-25 16:36:29 +0800

    authRouter axios url edit

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -3,43 +3,33 @@ import { loginUser, registerUser } from "../services/authService";
 import verifyToken from "../middlewares/verifyToken";
 const authRouter = Router();
 
-authRouter.post(
-  `${process.env.REACT_APP_API_URL}/api/register`,
-  async (req, res) => {
-    const { email, password, repassword, phone, name } = req.body;
+authRouter.post(`/api/register`, async (req, res) => {
+  const { email, password, repassword, phone, name } = req.body;
 
-    if (password !== repassword) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Password таарахгүй байнав" });
-    }
-    try {
-      await registerUser({ email, password, phone, name });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json(error);
-    }
+  if (password !== repassword) {
     return res
-      .status(200)
-      .json({ success: true, message: "Register successful" });
+      .status(400)
+      .json({ success: false, message: "Password таарахгүй байнав" });
   }
-);
+  try {
+    await registerUser({ email, password, phone, name });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+  return res
+    .status(200)
+    .json({ success: true, message: "Register successful" });
+});
 
-authRouter.post(
-  `${process.env.REACT_APP_API_URL}/api/signIn`,
-  async (req, res) => {
-    const { email, password } = req.body;
-    const response = await loginUser({ email, password });
-    res.status(response.status).json(response);
-  }
-);
+authRouter.post(`/api/signIn`, async (req, res) => {
+  const { email, password } = req.body;
+  const response = await loginUser({ email, password });
+  res.status(response.status).json(response);
+});
 
-authRouter.get(
-  `${process.env.REACT_APP_API_URL}/api/currentUser`,
-  verifyToken,
-  async (req, res) => {
-    return await res.status(200).json(req.user);
-  }
-);
+authRouter.get(`/api/currentUser`, verifyToken, async (req, res) => {
+  return await res.status(200).json(req.user);
+});
 
 export default authRouter;
